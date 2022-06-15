d330f3008dc98fc71c7ea965918cc949319c2dfa
asakundwi <mavhasa17@gmail.com>
2022-06-15 18:13:23 +0200

    added greet page

--- hello/views.py
+++ hello/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello There")
+    return render(request, "hello/index.html")
+
+def greet(request, name:str):
+    return render(request, "hello/greet.html",{
+        "name" : name.capitalize()
+    })
